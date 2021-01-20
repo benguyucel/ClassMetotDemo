802ba98977a277626e90ca256ccc8921b6465c0e
Yücel Bengü <taskoov@live.nl>
2021-01-20 09:30:35 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriMenager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriMenager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Eklendi");
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Silindi");
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+        }
+        public void musteriListele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Musteri Adı : "+musteri.MusteriAdi);
+                Console.WriteLine("Musteri Soy Adı : "+musteri.MusteriSoyadi);
+                Console.WriteLine("Musteri Yaşı : "+musteri.MusteriYasi);
+                Console.WriteLine("-----------------------------------");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri() { MusteriAdi = "Yücel",MusteriSoyadi="Bengü",MusteriYasi=29 };
+            Musteri musteri2 = new Musteri() { MusteriAdi = "Ali",MusteriSoyadi="Kahraman",MusteriYasi= 22 };
+
+            MusteriMenager musteriMenager = new MusteriMenager();
+            musteriMenager.Add(musteri2);
+            musteriMenager.Add(musteri1);
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            musteriMenager.musteriListele(musteriler);
+
+        }
+    }
+}
